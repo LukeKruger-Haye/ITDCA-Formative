42113c9508dd954b7ce51f631939bcd5e8f94796
Luke Kurger-Haye <eduv4777435@vossie.net>
2024-11-05 09:14:31 +0200

    Added Q1 and Q2 VS projects

---
+++ Question 1/Question 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Graphs; 
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+
+            graph.add_node(1);
+            graph.add_node(2);
+            graph.add_node(3);
+
+            graph.add_edge(1, 2, 5);
+            graph.add_edge(2, 3, 4);
+            graph.add_edge(3, 1, 9);
+
+            graph.print_dfs();
+        }
+    }
+}
---
+++ Question 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Graphs;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph("graph.txt");
+
+            graph.print_dfs();
+        }
+    }
+}
